3b662758d6d3119d9df68f90a467c41f64e0e330
Mikhail Polyakov PyCharm <mikhapol@bk.ru>
2023-04-15 14:21:40 +0300

    Создание конфига для JSON

---
+++ data/conf.py
@@ -0,0 +1,9 @@
+import os  # Подключение модуля OS - адаптация пути к файлам в ОС.
+
+BASE_PATH = os.path.abspath("data")  # Применение абсолютного пути.
+
+FILE_JSON = os.path.join(BASE_PATH, "operations.json")  # путь к файлу json с вопросами
+
+# print(BASE_PATH)
+# print(FILE_JSON)
+
